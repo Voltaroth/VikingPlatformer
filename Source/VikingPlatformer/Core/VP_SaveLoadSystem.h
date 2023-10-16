06de2a355cc9729a1257e2ec1a21f06ce67e3dad
MajerusS <135267494+MajerusS@users.noreply.github.com>
2023-10-16 17:22:17 +0200

    BP Save Game System, Set Up C++

---
+++ Source/VikingPlatformer/Core/VP_SaveLoadSystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "VP_SaveLoadSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIKINGPLATFORMER_API UVP_SaveLoadSystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
